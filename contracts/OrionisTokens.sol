e225f282465754cc67dc375e8571866ff0f87f1c
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2021-08-08 13:15:51 +0530

    Create OrionisTokens.sol

---
+++ contracts/OrionisTokens.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+
+contract OrionisTokens is ERC1155 {
+    uint256 public constant ONX = 0;
+    uint256 public constant ORN = 1;
+
+    constructor() public ERC1155("https://game.example/api/item/{id}.json") {
+    }
+    
+    function mintToken(address to, uint256 id, uint256 amount, bytes memory data) public virtual {
+        _mint(to, id, amount, data);
+    }
+    
+    function burnToken(address account, uint256 id, uint256 value) public virtual {
+        _burn(account, id, value);
+    }
+}
+
