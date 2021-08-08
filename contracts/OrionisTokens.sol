73d50b8c0067749ab803673af29efa53fa7cd439
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2021-08-08 20:09:08 +0530

    updated metadata url

--- contracts/OrionisTokens.sol
+++ contracts/OrionisTokens.sol
@@ -7,7 +7,7 @@ contract OrionisTokens is ERC1155 {
     uint256 public constant ONX = 0;
     uint256 public constant ORN = 1;
 
-    constructor() public ERC1155("https://game.example/api/item/{id}.json") {
+    constructor() public ERC1155("https://orionis-labs.github.io/orionis-on-eth/api/tokens/{id}.json") {
     }
     
     function mintToken(address to, uint256 id, uint256 amount, bytes memory data) public virtual {
